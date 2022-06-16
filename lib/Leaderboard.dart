7b64ca8e7c7cc54016463477164c70273c3fbea8
vegatama <vegatama23@gmail.com>
2022-06-16 15:56:31 +0700

    menambah api pada halaman leaderboard

--- lib/Leaderboard.dart
+++ lib/Leaderboard.dart
@@ -1,5 +1,8 @@
 import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 import 'package:flutter/material.dart';
+import 'leaderboard-1.dart';
+import 'leaderboard-2.dart';
+import 'leaderboard-3.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -86,71 +89,8 @@ class _ProfileState extends State<Profile> {
                 ),
               ),
             ),
-            body: TabBarView(children: [list(), list(), list()])));
-  }
-
-  SingleChildScrollView list() {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(20),
-            child: SizedBox(
-              height: 570,
-              child: ListView.separated(
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      tileColor: Color.fromARGB(150, 108, 108, 108),
-                      leading: Text("${index + 1}",
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 20),
-                          textAlign: TextAlign.center),
-                      title: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          const CircleAvatar(
-                            backgroundImage: NetworkImage(
-                                "https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1229&q=80"),
-                          ),
-                          SizedBox(width: 20),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              Text(
-                                "Abighail Shafira",
-                                style: TextStyle(
-                                    color: Colors.black, fontSize: 12),
-                              ),
-                              Text(
-                                "1900000",
-                                style: TextStyle(
-                                    color: Colors.black, fontSize: 12),
-                              ),
-                              Text(
-                                "Big Data, Data Visualization",
-                                style: TextStyle(
-                                    color: Colors.black, fontSize: 12),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      trailing: const Text("${(0)} Pts",
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                    );
-                  },
-                  separatorBuilder: (context, index) => const Divider(
-                        thickness: 0.5,
-                        color: Colors.white,
-                        indent: 1,
-                        endIndent: 1,
-                      ),
-                  itemCount: 10),
-            ),
-          )
-        ],
-      ),
-    );
+            body: TabBarView(children: [leaderboard1(), leaderboard2(), leaderboard3()])));
   }
 }
+
+
---
+++ lib/leaderboard-3.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class LeaderboardList {
+  String name;
+  String nip;
+  String pts;
+  String specialization;
+  String picturl;
+  LeaderboardList(
+      {required this.name,
+      required this.nip,
+      required this.pts,
+      required this.specialization,
+      required this.picturl});
+}
+
+class Leaderboard {
+  // ignore: non_constant_identifier_names
+  List<LeaderboardList> ListPop = <LeaderboardList>[];
+
+  Leaderboard(List<dynamic> json) {
+    // isi listPop disini
+    for (var i = 0; i < 50; i++) {
+      var name = json[i]["name"];
+      var nip = json[i]["nip"];
+      var pts = json[i]["pts"];
+      var specialization = json[i]["specialization"];
+      var picturl = json[i]["pict_url"];
+      ListPop.add(LeaderboardList(
+          name: name,
+          nip: nip,
+          pts: pts,
+          specialization: specialization,
+          picturl: picturl));
+    }
+  }
+  //map dari json ke atribut
+  factory Leaderboard.fromJson(List<dynamic> json) {
+    return Leaderboard(json);
+  }
+}
+
+// ignore: use_key_in_widget_constructors, camel_case_types
+class leaderboard3 extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+//class state
+class MyAppState extends State<leaderboard3> {
+  late Future<Leaderboard> futureLeaderboard;
+
+  String url =
+      "https://opensheet.elk.sh/1nTVgY_A1qE5wwZi76jh_5oSMWP5d11XSTjZfNhDa-3A/leaderboard-3";
+
+  //fetch data
+  Future<Leaderboard> fetchData() async {
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      // jika server mengembalikan 200 OK (berhasil),
+      // parse json
+      return Leaderboard.fromJson(jsonDecode(response.body));
+    } else {
+      // jika gagal (bukan  200 OK),
+      // lempar exception
+      throw Exception('Gagal load');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureLeaderboard = fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: FutureBuilder<Leaderboard>(
+        future: futureLeaderboard,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(20),
+                  child: SizedBox(
+                    height: 570,
+                    child: ListView.separated(
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          tileColor: Color.fromARGB(150, 108, 108, 108),
+                          leading: Text("${index + 1}",
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                              textAlign: TextAlign.center),
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              CircleAvatar(
+                                backgroundImage: NetworkImage(snapshot
+                                    .data!.ListPop[index].picturl
+                                    .toString()),
+                              ),
+                              SizedBox(width: 20),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    snapshot.data!.ListPop[index].name
+                                        .toString(),
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 12),
+                                  ),
+                                  Text(
+                                    snapshot.data!.ListPop[index].nip
+                                        .toString(),
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 12),
+                                  ),
+                                  Text(
+                                    snapshot.data!.ListPop[index].specialization
+                                        .toString(),
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 12),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          trailing: Text(
+                              snapshot.data!.ListPop[index].pts.toString(),
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                        );
+                      },
+                      separatorBuilder: (context, index) => const Divider(
+                        thickness: 0.5,
+                        color: Colors.white,
+                        indent: 1,
+                        endIndent: 1,
+                      ),
+                      itemCount: snapshot.data!.ListPop.length,
+                    ),
+                  ),
+                )
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          // By default, show a loading spinner.
+          return const CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+}
